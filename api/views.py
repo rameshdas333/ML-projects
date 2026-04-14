18b0c7596966825470468b7ed6a8e17cbdb14095
Ramesh Das <rameshdascse@gmail.com>
2026-04-14 20:54:06 +0600

    commit last push data

--- api/views.py
+++ api/views.py
@@ -1,34 +1,4 @@
-# # from django.shortcuts import render
 
-# # def home(request):
-# #     return render(request, "index.html")
-
-# import requests
-# from django.shortcuts import render
-
-# def home(request):
-#     result = None
-
-#     if request.method == "POST":
-#         engine_size = request.POST.get("engine_size")
-#         cylinders = request.POST.get("cylinders")
-#         fuel = request.POST.get("fuel_consumption_comb")
-
-#         response = requests.post(
-#             "http://127.0.0.1:8000/predict",
-#             json={
-#                 "engine_size": float(engine_size),
-#                 "cylinders": int(cylinders),
-#                 "fuel_consumption_comb": float(fuel)
-#             }
-#         )
-
-  
-
-#         if response.status_code == 200:
-#             result = response.json().get("CO2_Emission_Prediction")
-
-#     return render(request, "index.html", {"result": result})
 
 
 
